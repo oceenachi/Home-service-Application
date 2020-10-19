91a267f8782ac5e225e62e5c0b8c04a80a1a9fe1
oceenachi <phillipfrancisca@gmail.com>
2020-10-20 00:10:37 +0100

    create request repository to extend jpa repository

---
+++ src/main/java/com/order/repository/RequestRepository.java
@@ -0,0 +1,9 @@
+package com.order.repository;
+
+import com.order.model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+}
